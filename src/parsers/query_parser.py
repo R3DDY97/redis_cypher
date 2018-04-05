bfad0aa727ee07cbee4767db45e16015b7d7c978
r3ddy97 <venkyreddy097@gmail.com>
2018-04-05 08:59:34 +0530

    updated readme, refracted main src file

--- src/parsers/query_parser.py
+++ src/parsers/query_parser.py
@@ -2,8 +2,6 @@
 '''parses cypher query commands'''
 
 import os
-import re
-import json
 from redis_store import redis_db
 import re_utils
 
@@ -27,10 +25,12 @@ import re_utils
 
 class CypherGraph():
 
-    # clauses_dict = {"CREATE": self.parse_create, "MATCH": self.parse_match, "RETURN": self.parse_return, }
+    # clauses_dict = {"CREATE": self.parse_create,
+    #                 "MATCH": self.parse_match, "RETURN": self.parse_return, }
 
-    graph_nodes = []
-    graph_relations = []
+    graph_data = {"nodes": [], "relations": []}
+    cypher_nodes = []
+    cypher_relations = []
 
     # NODE_RGX = r"\(.*?\)"
     # RELATION_RGX = r"\[.*?\]"
@@ -47,33 +47,30 @@ class CypherGraph():
         # return clauses_dict[clause](query)
         if clause == "CREATE":
             return self.parse_create(query)
-
         if clause == "MATCH":
             return self.parse_match(query)
+        if clause == "SET":
+            return self.parse_set(query)
 
     def parse_create(self, query):
-        self.cypher_nodes = []
+        '''parses and stores cypher CREATE clause query request'''
         while True:
             node_parsed = self.parse_node(query)
             if node_parsed:
                 cypher_node, query = node_parsed
                 self.cypher_nodes.append(cypher_node)
+                self.graph_data["nodes"].append(cypher_node)
             if query and query[0] == ",":
                 query = query[1:].strip()
             else:
                 break
 
         if query and query[0] == "-":
-            return self.handle_relationship(query.strip("-"))
-            source_node = self.cypher_nodes[-1]
-            parsed_rltn = self.parse_relationship(query.strip("-"))
-            if not parsed_rltn:
-                print("Error in syntax")
-                os.sys.exit()
-            relationship, query = parsed_rltn
-            if query[0] == ">":
-                # directed relationship
-                parsed_target = self.parse_node(query[1:].strip())
+            query = self.handle_relationship(query.strip("-"))
+
+        if query and query[0] in "<-":
+            # incoming relation while making Path query .. will do later
+            pass
 
         if self.cypher_nodes:
             for node in self.cypher_nodes:
@@ -89,12 +86,15 @@ class CypherGraph():
         node, query = re_node
 
         re_property = re_utils.regex_property(node)
-        node_id, node_property = re_property or (node, {})
+        node_id, node_property = re_property or (node.strip(), {})
         node_name, *node_label = node_id.split(":")
-
-        cypher_node = [node_id, {"name": node_name, "label": node_label, "property": node_property,
-                                 "incoming_relns": {}, "outgoing_relns": {}, "undirected_relns": {}}]
-        self.graph_nodes.append(cypher_node)
+        mapping = {"name": node_name, "label": node_label,
+                   "property": node_property, "incoming_relns": [],
+                   "outgoing_relns": [], "undirected_relns": []}
+        cypher_node = ["node:{}".format(node_id), mapping]
+        self.graph_data["nodes"].append(cypher_node)
+        self.cypher_nodes.append(cypher_node)
+        # redis_db.add_node(cypher_node)
         return cypher_node, query
 
     def parse_relation(self, query):
@@ -104,40 +104,74 @@ class CypherGraph():
             return None
         relation, query = re_rltn
         re_property = re_utils.regex_property(relation)
-        reln_id, reln_property = re_property or (relation, {})
+        reln_id, reln_property = re_property or (relation.strip(), {})
         reln_name, *reln_label = reln_id.split(":")
-        cypher_reln = [reln_id, {"name": reln_name, "label": reln_label, "property": reln_property,
-                                 "source_node": {}, "target_node": {}, "undirected_nodes": {}}]
+        mapping = {"name": reln_name, "label": reln_label,
+                   "property": reln_property, "source_node": [],
+                   "target_node": [], "undirected_nodes": []}
+        cypher_reln = ["relation:{}".format(reln_id), mapping]
+        self.graph_data["relations"].append(cypher_reln)
+        self.cypher_relations.append(cypher_reln)
+        # redis_db.add_relation(cypher_reln)
         return cypher_reln, query
 
     def handle_relationship(self, query):
+        '''process cypher relationship between nodes'''
         source_node = self.cypher_nodes[-1]
         parsed_relation = self.parse_relation(query)
         if not parsed_relation:
             print("syntax error")
             os.sys.exit()
-        relation_entity, query = parsed_relation
-        if query[0] == ">":
-            # directed relationship
-            directionality = "directed"
+        cypher_reln, query = parsed_relation
+        directed = query[0] == ">"
+        if directed:            # directed relationship
             query = query.strip("> ")
-        else:
-            directionality = "undirected"
 
         parsed_node = self.parse_node(query)
         if not parsed_node:
-
             print("syntax error ----- 'NO target node found'")
+            os.sys.exit()
         target_node, query = parsed_node
-        return source_node, relation_entity, target_node, query
+        if directed:
+            self.directed_update(source_node, cypher_reln, target_node)
+        else:
+            self.undirected_update(source_node, cypher_reln, target_node)
+        return query
+
+    @classmethod
+    def directed_update(cls, source_node, cypher_reln, target_node):
+        '''update nodes, directed relations, entity and store in redis'''
+        cypher_reln[1]["target_node"].append(target_node)
+        cypher_reln[1]["source_node"].append(source_node)
+        source_node[1]["outgoing_relns"].append({"relation": cypher_reln, "node": target_node})
+        target_node[1]["incoming_relns"].append({"relation": cypher_reln, "node": source_node})
+        redis_db.add_entities([cypher_reln, source_node, target_node])
+
+    @classmethod
+    def undirected_update(cls, source_node, cypher_reln, target_node):
+        '''update nodes, undirected relations, entity and store in redis'''
+        cypher_reln[1]["undirected_nodes"].append(target_node, source_node)
+        source_node[1]["undirected_relns"].append({"relation": cypher_reln, "node": target_node})
+        target_node[1]["undirected_relns"].append({"relation": cypher_reln, "node": source_node})
+        redis_db.add_entities([cypher_reln, source_node, target_node])
+
+    def parse_set(self, query):
+        pass
 
     def parse_match(self, query):
         pass
 
+    def parse_return(self, query):
+        pass
+
     def execute_cypher(self, cypher_query):
+        ''''Takes cypher query as argument, parses and updates in redis-server'''
         query = cypher_query
         if not cypher_query[0] in ["'", '"']:
             return self.parse_query(query)
+        print("Syntax error")
+        return None
+
 
 
 # def main(self, ):
--- src/parsers/redis_store/redis_db.py
+++ src/parsers/redis_store/redis_db.py
@@ -10,7 +10,7 @@ R3DIS = redis.StrictRedis()
 
 def add_node(node_data):
     node_id, mapping = node_data  # mapping should have id, property, incoming n outgoing relations as keys
-    R3DIS.hmset(node_id, mapping)
+    R3DIS.hmset("node:{}".format(node_id), mapping)
     R3DIS.save()
 
 
@@ -20,4 +20,25 @@ def delete_node(node_id):
 
 
 def add_relation(relation_data):
-    source, relation, target = relation_data
+    reln_id, mapping = relation_data
+    R3DIS.hmset("relation:{}".format(reln_id), mapping)
+    R3DIS.save()
+
+
+def add_entity(entity_data):
+    if not entity_data:
+        print("Cant store empty entity in DB ")
+        return None
+    entity_id, mapping = entity_data
+    R3DIS.hmset(entity_id, mapping)
+    R3DIS.save()
+
+
+def add_entities(entity_list):
+    if not entity_list:
+        print("Cant store empty entity list in DB ")
+        return None
+    for entity in entity_list:
+        entity_id, mapping = entity
+        R3DIS.hmset(entity_id, mapping)
+    R3DIS.save()
