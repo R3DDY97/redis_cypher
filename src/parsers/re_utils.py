ea232a981be98b296b3c15e16f045136d60f95da
r3ddy97 <venkyreddy097@gmail.com>
2018-04-07 20:53:02 +0530

    improved regex soln for nodes n relations

--- src/parsers/re_utils.py
+++ src/parsers/re_utils.py
@@ -1,67 +1,50 @@
 #!/usr/bin/env python3
 '''helps in parsing and getting entities from cypher query statement using regexp'''
 
-
 import re
-import json
+# import json
 
 
-# writing clauses       = [CREATE, DELETE, DETACH_DELETE, SET, REMOVE]
-# projecting cluases    = [RETURN, WITH, UNWIND]
-# reading clauses       = [MATCH]
-# reading subclause     = [WHERE, SKIP, LIMIT, 'ORDER BY']
-# read_write_subclauses = [MERGE, CALL]
-
-# RE_NODE = re.compile(r"\(.*?\)")
-# RE_RELATION = re.compile(r"\[.*?\]")
-# RE_PROPERTY = re.compile(r"{.*?}")
+def regex_query(query):
+    '''searches cypher query and returns (tuple clause, sub_query) list'''
+    re_clause = re.findall("CREATE|MATCH|WHERE|RETURN", query)
+    re_query = re.split("CREATE|MATCH|WHERE|RETURN", query)[1:]
+    clause_list = [(c, regex_entities(q)) for c, q in zip(re_clause, re_query)]
+    return clause_list
 
 
-# NODE_RGX = r"\(.*?\)"
-# RELATION_RGX = r"\[.*?\]"
-# PROPERTY_RGX = r"{.*?}"
-# NODE_ID_RGX = r"([^{,])+"
-
-
-def regex_clause(query):
-    '''returns cypher clause if query satrts with any'''
-    re_clause = re.match(r"[a-zA-Z]+", query.strip())
-    if re_clause:
-        clause, query = re_clause.group(), query[re_clause.end():].strip()
-        return clause, query
-    else:
-        return None
-
-
-def regex_node(query):
-    re_node = re.match(r"\(.*?\)", query)
-    if re_node:
-        node, query = re_node.group()[1:-1], query[re_node.end():].strip()
-        return node, query
-    return None
-
-# def entity_object(self, entity):
-#     ''''takes cypher entity and returns its name:label, property'''
-#     re_property = regex_property(entity)
-#     if not re_property:
-#         return entity
-#     return re_property
+def regex_entities(clause_query):
+    matched_entities = re.findall(r"\(.*?\)(?!-)", clause_query)
+    relations = re.findall(r"\([\w,:'\s{}]+?\)-\[.*?\]->\(.*?\)", clause_query)
+    nodes = [item for item in matched_entities if item not in relations]
+    return nodes, relations
 
 
 def regex_property(entity):
     ''''regex to get property and id of cypher entity '''
-    re_property = re.search(r"{.*?}", entity)
-    if re_property:
-        entity_id, entity_property = entity[:re_property.start()], re_property.group().replace("'", '"')
-        entity_property = json.loads(entity_property)
-        return entity_id, entity_property
+    re_dict = re.search(r"{.*?}", entity)
+    if re_dict:
+        entity_id, entity_property = entity[1:re_dict.start()], re_dict.group()
+        entity_property = dict((i.split(":") for i in entity_property[1:-1].split(",")))
+        entity_property = fix_syntax(entity_property)
+        return entity_id.strip(), entity_property
     return None
 
 
-def regex_relation(query):
-    '''returns relation entity and query after parsing query'''
-    re_rltn = re.match(r"\[.*?\]", query)
-    if re_rltn:
-        relation, query = re_rltn.group()[1:-1], query[re_rltn.end():].strip("- ")
-        return relation, query
-    return None
+def fix_syntax(entity_property):
+    '''returns input dict with parsing numbers'''
+    entity_property = {k.strip(" '\""): v.strip() for k, v in entity_property.items()}
+    for k, v in entity_property.items():
+        if not "'" in v or '"' in v:
+            entity_property[k] = str2number(v)
+        else:
+            entity_property[k] = v.strip(" '\"")
+    return entity_property
+
+
+def str2number(string):
+    try:
+        number = int(string)
+    except ValueError:
+        number = float(string)
+    return number
